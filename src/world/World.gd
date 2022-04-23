cc4ad65ffc1ed70f6beafd7deb11c56c3129bd62
Your Name <silver1miner@gmail.com>
2022-04-22 20:12:43 -0700

    physics objects, posters

--- src/world/World.gd
+++ src/world/World.gd
@@ -10,3 +10,14 @@ func _on_Exit_body_entered(body: Node) -> void:
 func _on_StartTrigger_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		PlayerData.game_started = true
+
+
+func _on_SignTrigger_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		randomize()
+		if rand_range(1,10) > 7:
+			$Signs/OfficePoster.visible = false
+			$Signs/OfficePoster2.visible = true
+		else:
+			$Signs/OfficePoster.visible = true
+			$Signs/OfficePoster2.visible = false
